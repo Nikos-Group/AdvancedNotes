e3701e7cad0fd94508e034b21db36c9783211ca8
NikitaTropin3536 <Tropinikita.06@mail.ru>
2023-10-04 23:14:30 +0300

    Add helper function that is necessary for displaying toast messages

---
+++ app/src/main/java/com/ariete/advancednotes/helper/ShowToast.kt
@@ -0,0 +1,25 @@
+package com.ariete.advancednotes.helper
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(message: String) {
+    Toast.makeText(
+        this,
+        message,
+        Toast.LENGTH_SHORT
+        /**
+            * Toast.LENGTH_SHORT is a constant representing
+            * a predefined duration for displaying a Toast message.
+            *
+            * Toast.LENGTH_SHORT means that the Toast message will be displayed
+            * for a short period of time, which is approximately 2 seconds.
+            * -----------------------------------------------------------------
+            * Toast.LENGTH_SHORT - константа, представляющая
+            * (предопределенную продолжительность) некоторый период времени для отображения toast.
+            *
+            * Toast.LENGTH_SHORT значит, что toast будет отображаться в течение
+            * небольшого промежутка времени - примерно 2 секунды.
+        */
+    ).show()
+}
