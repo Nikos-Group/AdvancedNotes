c41c0a302eac6412350bf5652bf4ba9a5338b9f3
NikitaTropin3536 <Tropinikita.06@mail.ru>
2023-10-08 00:13:12 +0300

    Add logic for FragmentSettings and layout to fragment_settings.xml file

--- app/src/main/java/com/ariete/advancednotes/fragments/SettingsFragment.kt
+++ app/src/main/java/com/ariete/advancednotes/fragments/SettingsFragment.kt
@@ -1,60 +1,118 @@
 package com.ariete.advancednotes.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.ariete.advancednotes.R
+import com.ariete.advancednotes.activities.MainActivity
+import com.ariete.advancednotes.databinding.FragmentSettingsBinding
+import com.ariete.advancednotes.databinding.LayoutAboutUsBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class SettingsFragment :
+    Fragment(R.layout.fragment_settings),
+    View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var mView: View
+
+    private lateinit var bottomNavigation: BottomNavigationView
+
+    private lateinit var aboutUs: CardView
+
+    private lateinit var dialogAboutUs: AlertDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View {
+
+        _binding = FragmentSettingsBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        animation()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mView = view
+
+        assign_bottomNavigation_item()
+
+        aboutUs = binding.aboutUs
+        setting_up_click_aboutUs()
+        setting_up_dialogAboutUs()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun animation() {
+        lifecycleScope.launch {
+            binding.root.alpha = 0f
+
+            delay(500)
+
+            binding.root
+                .animate()
+                .alpha(1f)
+                .duration = 250
+        }
+    }
+
+    private fun `assign_bottomNavigation_item`() {
+        bottomNavigation = (activity as MainActivity).bottomNavigation
+        bottomNavigation
+            .menu
+            .findItem(R.id.settings)
+            .isChecked = true
+    }
+
+    private fun `setting_up_click_aboutUs`() {
+        aboutUs.setOnClickListener(this)
+    }
+
+    private fun `setting_up_dialogAboutUs`() {
+        val builder = AlertDialog.Builder(context)
+
+        val view = LayoutAboutUsBinding.inflate(
+            LayoutInflater.from(context)
+        )
+
+        builder.setView(view.root)
+        dialogAboutUs = builder.create()
+
+        dialogAboutUs.window!!.setBackgroundDrawableResource(
+            R.drawable.background_dialog_about_us
+        )
+
+        view.ok.setOnClickListener {
+            dialogAboutUs.dismiss()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            binding.aboutUs -> {
+                dialogAboutUs.show()
             }
+        }
     }
 }
